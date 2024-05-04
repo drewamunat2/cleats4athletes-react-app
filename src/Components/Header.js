e6888d9502d4621b9edc28282a0a0235c3f8d0f3
Drew Amunategui II <drewtegui@Drews-MBP.hsd1.md.comcast.net>
2024-05-03 21:48:55 -0400

    no more donate now button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,298 +20,278 @@ export default function Header() {
     return (
       <AppBar position="sticky" elevation={0} sx={{ py: 2 }}>
         <Toolbar>
-            <Grid
-              container
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Grid item justifyContent="center" alignItems="center" xs={2}>
-                <Card sx={{ bgcolor: "transparent" }}>
-                  <Link href="/">
-                    <CardMedia
-                      component="img"
-                      image={Icon}
-                      sx={{
-                        height: 90,
-                        width: 90,
-                      }}
-                    ></CardMedia>
-                  </Link>
-                </Card>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item justifyContent="center" alignItems="center" xs={2}>
+              <Card sx={{ bgcolor: "transparent" }}>
+                <Link href="/">
+                  <CardMedia
+                    component="img"
+                    image={Icon}
+                    sx={{
+                      height: 90,
+                      width: 90,
+                    }}
+                  ></CardMedia>
+                </Link>
+              </Card>
+            </Grid>
+            <Grid item justifyContent="center" alignItems="center" xs={10}>
+              {/* mobile */}
+              <Grid
+                container
+                spacing={0}
+                justifyContent="center"
+                alignItems="center"
+                sx={{
+                  overflowY: "scroll",
+                  display: { xs: "flex", md: "none" },
+                }}
+              >
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/about"
+                  >
+                    About
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/team"
+                  >
+                    Team
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/awards"
+                  >
+                    Awards
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/testimonials"
+                  >
+                    Testimonials
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/news"
+                  >
+                    News
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/contact"
+                  >
+                    Contact Us
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="small"
+                    variant="text"
+                    color="inherit"
+                    href="/events"
+                  >
+                    Events
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item justifyContent="center" alignItems="center" xs={10}>
-                {/* mobile */}
-                <Grid
-                  container
-                  spacing={0}
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{
-                    overflowY: "scroll",
-                    display: { xs: "flex", md: "none" },
-                  }}
-                >
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/about"
-                    >
-                      About
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/team"
-                    >
-                      Team
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/awards"
-                    >
-                      Awards
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/testimonials"
-                    >
-                      Testimonials
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/news"
-                    >
-                      News
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/contact"
-                    >
-                      Contact Us
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/events"
-                    >
-                      Events
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="small"
-                      variant="text"
-                      color="inherit"
-                      href="/donate"
-                    >
-                      Donate Now
-                    </Button>
-                  </Grid>
+              {/*ipad*/}
+              <Grid
+                container
+                spacing={0}
+                justifyContent="center"
+                alignItems="center"
+                sx={{ display: { xs: "none", md: "flex", lg: "none" } }}
+              >
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/about"
+                  >
+                    About
+                  </Button>
                 </Grid>
-                {/*ipad*/}
-                <Grid
-                  container
-                  spacing={0}
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{ display: { xs: "none", md: "flex", lg: "none" } }}
-                >
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/about"
-                    >
-                      About
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/team"
-                    >
-                      Team
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/awards"
-                    >
-                      Awards
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/testimonials"
-                    >
-                      Testimonials
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/news"
-                    >
-                      News
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/events"
-                    >
-                      Events
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/contact"
-                    >
-                      Contact Us
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/donate"
-                    >
-                      Donate Now
-                    </Button>
-                  </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/team"
+                  >
+                    Team
+                  </Button>
                 </Grid>
-                {/*laptop*/}
-                <Grid
-                  container
-                  spacing={3}
-                  justifyContent="center"
-                  alignItems="center"
-                  sx={{ display: { xs: "none", lg: "flex" } }}
-                >
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/about"
-                    >
-                      About
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/team"
-                    >
-                      Team
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/awards"
-                    >
-                      Awards
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/testimonials"
-                    >
-                      Testimonials
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/news"
-                    >
-                      News
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/events"
-                    >
-                      Events
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/contact"
-                    >
-                      Contact Us
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button
-                      size="medium"
-                      variant="text"
-                      color="inherit"
-                      href="/donate"
-                    >
-                      Donate Now
-                    </Button>
-                  </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/awards"
+                  >
+                    Awards
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/testimonials"
+                  >
+                    Testimonials
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/news"
+                  >
+                    News
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/events"
+                  >
+                    Events
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/contact"
+                  >
+                    Contact Us
+                  </Button>
+                </Grid>
+              </Grid>
+              {/*laptop*/}
+              <Grid
+                container
+                spacing={3}
+                justifyContent="center"
+                alignItems="center"
+                sx={{ display: { xs: "none", lg: "flex" } }}
+              >
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/about"
+                  >
+                    About
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/team"
+                  >
+                    Team
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/awards"
+                  >
+                    Awards
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/testimonials"
+                  >
+                    Testimonials
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/news"
+                  >
+                    News
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/events"
+                  >
+                    Events
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/contact"
+                  >
+                    Contact Us
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    size="medium"
+                    variant="text"
+                    color="inherit"
+                    href="/donate"
+                  >
+                    Donate Now
+                  </Button>
                 </Grid>
               </Grid>
             </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     );
